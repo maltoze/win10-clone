c312787f48da98ea5b801e3ff0971da556ca4b24
maltoze <maltoze@qq.com>
2022-07-21 16:13:12 +0800

    fix: use default cursor

--- src/apps/chrome/components/IconButton.tsx
+++ src/apps/chrome/components/IconButton.tsx
@@ -3,7 +3,7 @@ import { IconButtonProps } from '../../../types';
 const IconButton = ({ icon, ...props }: IconButtonProps) => {
   return (
     <button
-      className="rounded-full p-1 hover:bg-zinc-600 disabled:bg-zinc-700"
+      className="cursor-default rounded-full p-1 hover:bg-zinc-600 disabled:bg-zinc-700"
       {...props}
     >
       {icon}
--- src/components/base/buttons/WindowCloseButton.tsx
+++ src/components/base/buttons/WindowCloseButton.tsx
@@ -4,7 +4,10 @@ const WindowCloseButton = (
   props: React.ButtonHTMLAttributes<HTMLButtonElement>
 ) => {
   return (
-    <button className="px-3.5 py-1.5 hover:bg-red-700" {...props}>
+    <button
+      className="cursor-default px-3.5 py-1.5 hover:bg-red-700"
+      {...props}
+    >
       <WindowCloseIcon className="h-[1.125rem] w-[1.125rem] text-zinc-300" />
     </button>
   );
