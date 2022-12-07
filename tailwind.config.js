139508f346ba338abefbebf7ec478a3db635dc54
maltoze <maltoze@qq.com>
2022-12-07 10:37:50 +0800

    chore: add color zinc 750

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        zinc: {
+          750: '#303036',
+        },
+      },
+    },
   },
   plugins: [],
 };
