c3151331553240e9e06b93e45b66f3533d01f1c1
maltoze <maltoze@qq.com>
2023-02-22 11:12:16 +0800

    feat(chrome): add google logo

--- src/apps/chrome/index.tsx
+++ src/apps/chrome/index.tsx
@@ -37,7 +37,9 @@ const Chrome = () => {
         <Tab.Panel className="h-full">
           <div className="flex h-full flex-col bg-zinc-700">
             <AddressBar />
-            <div className="grow border-t border-zinc-500 bg-zinc-800 bg-opacity-70"></div>
+            <div className="flex grow justify-center border-t border-zinc-500 bg-zinc-800 bg-opacity-70 pt-14">
+              <div className="[mask-image:url(/icons/google-logo.svg)] w-[272px] h-[92px] mb-10 mt-20 bg-white"></div>
+            </div>
           </div>
         </Tab.Panel>
       </Tab.Panels>
